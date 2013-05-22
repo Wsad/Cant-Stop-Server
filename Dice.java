dcaa1e1e29a17342f2ee9d9b6f7c0a7658cd49a7
TheNigerian <nigel.gauvin@gmail.com>
2013-05-22 11:45:13 -0300

    added GetDice method

--- Dice.java
+++ Dice.java
@@ -4,12 +4,15 @@ public class Dice {
 	int d1,d2,d3,d4;
 	
 	public static void main (String[]args){
+		
 		Dice d1 = new Dice ();
 		d1.roll();
 		d1.split();
+		System.out.println(d1.getDice());
 											}
 	
 	public Dice () {
+		
 			d1 = 0;
 			d2 = 0;
 			d3 = 0;
@@ -19,13 +22,15 @@ public class Dice {
 	
 	public void roll (){
 		//**Create a random object to act as a dice
-	Random dice1 = new Random();	
+		Random dice1 = new Random();	
 
-		
+		//Mark the range
 		int START = 1;
 		int END = 6;
+		
 		//** Create a range for the random numbers
 		long range = (long)END - (long)START + 1;
+		
 		 // compute a fraction of the range, 0 <= frac < range
 	    long fraction1 = (long)(range * dice1.nextDouble());
 	    d1 =  (int)(fraction1 + START);
@@ -36,23 +41,28 @@ public class Dice {
 	    long fraction4 = (long)(range * dice1.nextDouble());
 	    d4 =  (int)(fraction4 + START);
 	
-		
-		System.out.println(d1 + " " + d2 + " " + d3 + " " + d4 );
 						}
 	
 		public void split(){
-			 int split1 = d1 + d4; 
-			 int halfsplit1 = d2 + d3;
-			 
-			 int split2 = d1 + d3;
-			 int halfsplit2 = d2 + d4;
-			 
-			 int split3 = d1 + d2;
-			 int halfsplit3 = d3 + d4;
-			 
-			 System.out.println("(" + split1 + "," + halfsplit1 + ")" + " (" + split2 + "," + halfsplit2 + ")" + " (" + split3 + "," + halfsplit3 + ")");
 			
-																	}
+		int split1 = d1 + d4; 
+		int halfsplit1 = d2 + d3;
+			 
+		int split2 = d1 + d3;
+		int halfsplit2 = d2 + d4;
+			 
+		int split3 = d1 + d2;
+		int halfsplit3 = d3 + d4;
+			 
+		System.out.println("(" + split1 + "," + halfsplit1 + ")" + " (" + split2 + "," + halfsplit2 + ")" + " (" + split3 + "," + halfsplit3 + ")");
+			
+							}
+		
+		public String getDice(){
+			String text = " " + d1 + " " + d2 + " " + d3 + " " + d4;  
+			return text;
+				
+								}
 	
 	
 
