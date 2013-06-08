0c7aa5c842f0859d7b5c8ad2521f10f96f29b47f
TheNigerian <nigel.gauvin@gmail.com>
2013-06-08 16:24:44 -0300

    added javadocs to Board, Dice and PlayerInfo

--- PlayerInfo.java
+++ PlayerInfo.java
@@ -1,17 +1,24 @@
+/** @author Alex Ringeri PlayerInfo Class */
+
 import java.io.Serializable;
 
 public class PlayerInfo implements Serializable {
 	private int wins, losses, games;
 	private String password;
 	private String username;
-	
+
+	/** Constructor: creates PlayerInfo object accepts the password entered and sets 	*		 wins, losses and games to 0.
+	*		 @param  passwordIn 	-represents the password player enters	*/	
 	public PlayerInfo(String passwordIn){
 		password = passwordIn;
 		wins = 0;
 		losses = 0;
 		games = 0;
 	}
-	
+
+	/** Method getPassword to retrieve the password 
+	*	   @param void
+	*	   @return String password 	     */
 	public String getPassword(){
 		return password;
 	}
